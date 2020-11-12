6ddea2af1dcc953183a5119c2a2ee4ab376deb28
RaymondBello <raymondbello11@gmail.com>
2020-11-11 21:21:15 -0500

    update

--- Ground Control Software/Python Telemetry Dashboard/GCS_START.bat
+++ Ground Control Software/Python Telemetry Dashboard/GCS_START.bat
@@ -1,3 +1,3 @@
-start  C:/Users/raymo/AppData/Local/Programs/Python/Python37/python.exe "GCS_PLOT.py" 
+start  python3 "GCS_PLOT.py" 
 
-start  C:/Users/raymo/AppData/Local/Programs/Python/Python37/python.exe "GCS_PFD.py"
+
