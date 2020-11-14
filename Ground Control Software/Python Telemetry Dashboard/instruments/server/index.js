c922dc6b2ac0d1c82b63d2979fce7caec24bfd57
RaymondBello <raymondbello11@gmail.com>
2020-11-14 08:01:03 -0500

    added typechecking and local file storage

--- Ground Control Software/Python Telemetry Dashboard/instruments/server/index.js
+++ Ground Control Software/Python Telemetry Dashboard/instruments/server/index.js
@@ -1,12 +1,23 @@
 const WebSocket = require("ws");
+var fs = require('fs');
+var rawData = fs.readFileSync('../sensor_data.json');
+var JSONdata = JSON.parse(rawData);
 
 const wss = new WebSocket.Server({ port: 8082 });
+
 
 wss.on("connection", ws => {
     console.log("New Client Connected...");
 
     ws.on("message", data => {
-        console.log("[RECEIVED] " + data);
+
+        try {
+            var data_ = JSON.parse(data);
+            saveToJSON(data_);
+            
+        } catch (e) {
+            console.log(e.message);
+        }
         
         ws.send("[INFO] Data received by localhost");
     });
@@ -15,3 +26,21 @@ wss.on("connection", ws => {
         console.log("Client Has disconnected.");
     })
 });
+
+/**
+ * Saves JSON data to a local file.
+ *
+ * @param {JSON} data - Raw data to be saved into JSON file
+ */
+function saveToJSON(data) {
+    console.log("[RECEIVED]", data);
+
+    fs.writeFile('../trajectory.json', JSON.stringify(data), finished);
+
+    function finished(err) {
+        console.log('Wrote JSON file');
+    };
+    
+}
+
+console.log(JSONdata);
