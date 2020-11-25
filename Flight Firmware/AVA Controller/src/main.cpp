bbbae61aa378be1d7ce0f7d1fa2b31ecc5bdf387
RaymondBello <raymondbello11@gmail.com>
2020-11-25 17:21:52 -0500

    controller

--- Flight Firmware/AVA Controller/src/main.cpp
+++ Flight Firmware/AVA Controller/src/main.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 #include <Wire.h>
-#include <WebSocketsServer.h>
 #include <MPU9250.h>
 #include <Adafruit_BMP280.h>
+#include "kalman_filter.cpp"
 
 #define BMP_SCK (13)
 #define BMP_MISO (12)
@@ -24,89 +22,11 @@ float sample_rate;
 
 MPU9250 mpu;
 Adafruit_BMP280 bmp; // I2C
-WebSocketsServer webSocket = WebSocketsServer(80);
 
-// State Constants
-const int CURRENT_STATE = 0;
-int STATE_POOL[7] = {0, 1, 2, 3, 4, 5, 6};
 static uint32_t t_delta = 0;
 uint32_t t_start = 0;
 
-const char *ssid = "baca";
-const char *password = "randy053";
-
-char buffer_udp_out[150];
-char buffer_udp_in[150];
 char buffer_serial_out[150];
-
-// Debug Enum Flags
-enum debugFlags
-{
-  DEBUG = 1,
-  INFO = 2,
-  WARN = 4,
-  ERROR = 8,
-  FATAL = 16
-};
-
-void serialDebug(enum debugFlags level, const char *msg)
-{
-  switch (level)
-  {
-  case 1:
-    Serial.printf("[DEBUG] %s \n", msg);
-    break;
-  case 2:
-    Serial.printf("[INFO] %s \n", msg);
-    break;
-  case 4:
-    Serial.printf("[WARN] %s \n", msg);
-    break;
-  case 8:
-    Serial.printf("[ERROR] %s \n", msg);
-    break;
-  case 16:
-    Serial.printf("[FATAL] %s \n", msg);
-  }
-}
-// Called when receiving websocket packet
-void onWebSocketEvent(u_int num, WStype_t type, uint8_t *payload, size_t length)
-{
-
-  // Determine type of Websocket event
-  switch (type)
-  {
-  case (WStype_DISCONNECTED):
-  {
-    // Serial.printf("[%u] Disconnected.\n", num);
-    break;
-  }
-  case (WStype_CONNECTED):
-  {
-    IPAddress ip = webSocket.remoteIP(num);
-    // Serial.printf("[%u] Connection from ", num);
-    Serial.println(ip.toString());
-    break;
-  }
-  case (WStype_TEXT):
-  {
-    sprintf(buffer_udp_in, "%s", payload);
-    // Serial.println(buffer_udp_in);
-
-    sprintf(buffer_udp_out, "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f", acc[0], acc[1], acc[2], gyro[0], gyro[1], gyro[2], mag[0], mag[1], mag[2], qrt[0], qrt[1], qrt[2], qrt[3], pressure, altitude, temp, pitch, roll, yaw);
-    webSocket.sendTXT(num, buffer_udp_out);
-    break;
-  }
-
-  case (WStype_BIN):
-  case (WStype_ERROR):
-  case (WStype_FRAGMENT_TEXT_START):
-  case (WStype_FRAGMENT):
-  case (WStype_FRAGMENT_FIN):
-  default:
-    break;
-  }
-}
 
 void I2C_sensors_begin()
 {
@@ -124,41 +44,6 @@ void I2C_sensors_begin()
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-}
-
-void init_WiFi()
-{
-  WiFi.begin(ssid, password);
-
-  Serial.print("[READY] : Connecting to ");
-  Serial.println(ssid);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.println("Connected. On");
-  // Serial.print("My Address: ");
-  Serial.print(WiFi.localIP());
-  Serial.println();
-
-  // Start Websocket server
-  webSocket.begin();
-  webSocket.onEvent(onWebSocketEvent);
-}
-
-void serial_print_GPS_values()
-{
-  // while (GPSserial.available()>0)
-  // {
-  //   Serial.write(GPSserial.read());
-  // }
-}
-
-void start_websocket_loop()
-{
-  webSocket.loop();
 }
 
 void IMU_update()
@@ -179,8 +64,8 @@ void IMU_update()
     lin_a[2] = mpu.getLinAccZ();
 
     gyro[0] = mpu.getGyro(0) - g_offset[0];
-    gyro[1] = mpu.getGyro(1)-g_offset[1];
-    gyro[2] = mpu.getGyro(2)- g_offset[2];
+    gyro[1] = mpu.getGyro(1) - g_offset[1];
+    gyro[2] = mpu.getGyro(2) - g_offset[2];
 
     mag[0] = mpu.getMag(0);
     mag[1] = mpu.getMag(1);
@@ -199,22 +84,6 @@ void IMU_update()
   }
 }
 
-void baro_update()
-{
-  static uint32_t prev_ms = millis();
-
-  if ((millis() - prev_ms) > 0)
-  {
-    temp = bmp.readTemperature();
-    pressure = bmp.readPressure();
-    altitude = bmp.readAltitude(1026);
-
-    prev_ms = millis();
-  }
-  
-
-}
-
 void IMU_calibrate()
 {
   /**
@@ -224,10 +93,10 @@ void IMU_calibrate()
   mpu.calibrateAccelGyro();
   // delay(50);
   // mpu.calibrateMag();
-  // serialDebug(INFO, "IMU is calibrated");
 
   float g_raw[3]; //, a_raw[3] ;
   int i;
+
   for (i = 0; i < 100; i++)
   {
     mpu.update();
@@ -265,9 +134,23 @@ void IMU_calibrate()
   // Serial.println(" ");
 }
 
+void baro_update()
+{
+  static uint32_t prev_ms = millis();
+
+  if ((millis() - prev_ms) > 1)
+  {
+    temp = bmp.readTemperature();
+    pressure = bmp.readPressure();
+    altitude = bmp.readAltitude(1026);
+
+    prev_ms = millis();
+  }
+}
+
 void setup()
 {
-  system_update_cpu_freq(160);
+  // put your setup code here, to run once:
   Serial.begin(Serial_Monitor_Baud);
   while (!Serial)
   {
@@ -275,29 +158,30 @@ void setup()
   }
   I2C_sensors_begin();
   IMU_calibrate();
-  init_WiFi();
   Wire.setClock(400000L);
 }
 
 void loop()
 {
+  // put your main code here, to run repeatedly:
   t_start = millis();
   /*******************************
    *  Sensor update functions
    *******************************/
   IMU_update();
+
+  // Kalman filter Here
+
   baro_update();
 
-  t_delta = millis()- t_start;
+  t_delta = millis() - t_start;
 
-  if (t_delta>1){
+  if (t_delta > 1)
+  {
     sample_rate = (float)t_delta;
     sample_rate = 1 / (sample_rate / 1000);
   }
 
-  sprintf(buffer_serial_out, "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%.1f", lin_a[0], lin_a[1], lin_a[2], gyro[0], gyro[1], gyro[2], mag[0], mag[1], mag[2], qrt[0], qrt[1], -qrt[2], qrt[3], pressure, altitude, temp, pitch, roll, yaw, t_delta,sample_rate);
+  sprintf(buffer_serial_out, "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%.1f", lin_a[0], lin_a[1], lin_a[2], gyro[0], gyro[1], gyro[2], mag[0], mag[1], mag[2], qrt[0], qrt[1], -qrt[2], qrt[3], pressure, altitude, temp, pitch, roll, yaw, t_delta, sample_rate);
   Serial.println(buffer_serial_out);
-
-  start_websocket_loop();
-  // delay(10);
 }
