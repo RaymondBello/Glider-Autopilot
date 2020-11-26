73aae10a609f91be36e257df5a9712bf58817834
RaymondBello <raymondbello11@gmail.com>
2020-11-26 01:35:10 -0500

    SPI

--- Flight Firmware/AVA Controller/src/main.cpp
+++ Flight Firmware/AVA Controller/src/main.cpp
@@ -2,20 +2,20 @@
 #include <Wire.h>
 #include <MPU9250.h>
 #include <Adafruit_BMP280.h>
-#include "kalman_filter.cpp"
+#include "kalman_filter.h"
 
 #define BMP_SCK (13)
 #define BMP_MISO (12)
 #define BMP_MOSI (11)
 #define BMP_CS (10)
 
-#define GPSBaud 9600 //GPS Baud rate
 #define Serial_Monitor_Baud 115200
 
 float a_offset[3], g_offset[3];
 float acc[3], gyro[3], mag[3];
 float lin_a[3];
 float qrt[4] = {1.0f, 0.0f, 0.0f, 0.0f};
+double acc_k[3], gyro_k[3], mag_k[3];
 float pitch, yaw, roll;
 float temp, pressure, altitude;
 float sample_rate;
@@ -26,7 +26,7 @@ Adafruit_BMP280 bmp; // I2C
 static uint32_t t_delta = 0;
 uint32_t t_start = 0;
 
-char buffer_serial_out[150];
+char buffer_serial_out[200];
 
 void I2C_sensors_begin()
 {
@@ -94,7 +94,8 @@ void IMU_calibrate()
   // delay(50);
   // mpu.calibrateMag();
 
-  float g_raw[3]; //, a_raw[3] ;
+  float g_raw[3] = {};
+  //, a_raw[3] ;
   int i;
 
   for (i = 0; i < 100; i++)
@@ -148,6 +149,22 @@ void baro_update()
   }
 }
 
+// Updates all the Kalman filter values
+void update_kalman_filter()
+{
+  acc_k[0] = kalman_ax((double)lin_a[0]);
+  acc_k[1] = kalman_ay((double)lin_a[1]);
+  acc_k[2] = kalman_az((double)lin_a[2]);
+
+  gyro_k[0] = kalman_gx((double)gyro[0]);
+  gyro_k[1] = kalman_gy((double)gyro[1]);
+  gyro_k[2] = kalman_gz((double)gyro[2]);
+
+  mag_k[0] = kalman_mx((double)mag[0]);
+  mag_k[1] = kalman_my((double)mag[1]);
+  mag_k[2] = kalman_mz((double)mag[2]);
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -163,25 +180,29 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // Start timer
   t_start = millis();
+
   /*******************************
    *  Sensor update functions
    *******************************/
-  IMU_update();
-
-  // Kalman filter Here
-
-  baro_update();
+  baro_update();          // Barometer Update
+  IMU_update();           // Update IMU values
+  update_kalman_filter(); // Kalman filter updates
 
   t_delta = millis() - t_start;
-
   if (t_delta > 1)
   {
     sample_rate = (float)t_delta;
     sample_rate = 1 / (sample_rate / 1000);
   }
 
-  sprintf(buffer_serial_out, "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%.1f", lin_a[0], lin_a[1], lin_a[2], gyro[0], gyro[1], gyro[2], mag[0], mag[1], mag[2], qrt[0], qrt[1], -qrt[2], qrt[3], pressure, altitude, temp, pitch, roll, yaw, t_delta, sample_rate);
+  /*
+  sprintf(buffer_serial_out, "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%.1f, %.3f, %.3f, %.3f", lin_a[0], lin_a[1], lin_a[2], gyro[0], gyro[1], gyro[2], mag[0], mag[1], mag[2], qrt[0], qrt[1], -qrt[2], qrt[3], pressure, altitude, temp, pitch, roll, yaw, t_delta, sample_rate, acc_k[0], acc_k[1], acc_k[2]);
   Serial.println(buffer_serial_out);
+  */
+
+  Serial.printf("%.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f", sample_rate, acc_k[0], acc_k[1], acc_k[2], gyro_k[0], gyro_k[1], gyro_k[2], mag_k[0], mag_k[1], mag_k[2]);
+  Serial.println();
+  // delay(500);
 }
