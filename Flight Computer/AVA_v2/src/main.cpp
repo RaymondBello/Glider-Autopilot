694e003fe2bbf13b42de72c70a702d460e0d9d9a
RaymondBello <raymondbello11@gmail.com>
2021-02-06 05:04:29 -0500

    teensy start

---
+++ Flight Computer/AVA_v2/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print("TESTING TEENSY");
+}
