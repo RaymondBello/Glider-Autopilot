9732a911965f0332f24753ef4228899d91baf603
RaymondBello <raymondbello11@gmail.com>
2021-02-28 17:31:23 -0500

    added chibiOS

--- Flight Computer/AVA_v3/lib/ChRt/examples/chBlinkPrint/chBlinkPrint.ino
+++ Flight Computer/AVA_v3/lib/ChRt/examples/chBlinkPrint/chBlinkPrint.ino
@@ -1,12 +1,15 @@
+// Simple demo of three threads.
+// LED blink thread, count thread, and main thread.
 #include "ChRt.h"
-#include "config.h"
+
+// LED_BUILTIN pin on Arduino is usually pin 13.
 
 volatile uint32_t count = 0;
-
 //------------------------------------------------------------------------------
 // thread 1 - high priority for blinking LED.
 // 64 byte stack beyond task switch and interrupt needs.
 static THD_WORKING_AREA(waThread1, 64);
+
 static THD_FUNCTION(Thread1 , arg) {
   (void)arg;
 
@@ -27,11 +30,11 @@ static THD_FUNCTION(Thread1 , arg) {
     chThdSleepMilliseconds(150);
   }
 }
-
 //------------------------------------------------------------------------------
 // thread 2 - count when higher priority threads sleep.
 // 64 byte stack beyond task switch and interrupt needs.
 static THD_WORKING_AREA(waThread2, 64);
+
 static THD_FUNCTION(Thread2, arg) {
   (void)arg;
 
@@ -41,8 +44,8 @@ static THD_FUNCTION(Thread2, arg) {
     interrupts();
   }
 }
-
-
+//------------------------------------------------------------------------------
+// Name chSetup is not special - must be same as used in chBegin() call.
 void chSetup() {
   // Start blink thread. Priority one more than loop().
   chThdCreateStatic(waThread1, sizeof(waThread1),
@@ -51,9 +54,9 @@ void chSetup() {
   chThdCreateStatic(waThread2, sizeof(waThread2),
                     NORMALPRIO - 1, Thread2, NULL);
 }
-
+//------------------------------------------------------------------------------
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   // Wait for USB Serial.
   while (!Serial) {}
 
@@ -61,29 +64,30 @@ void setup() {
   // chBegin() resets stacks and should never return.
   while (true) {}
 }
-
+//------------------------------------------------------------------------------
+// Runs at NORMALPRIO.
 void loop() {
-    // Runs at NORMALPRIO.
-    chThdSleepMilliseconds(1000);
-    
-    // Print count for previous second.
-    Serial.print(F("Count: "));
-    Serial.print(count);
-    
-    // Zero count.
-    count = 0;
+  // Sleep for one second.
+  chThdSleepMilliseconds(1000);
+  
+  // Print count for previous second.
+  Serial.print(F("Count: "));
+  Serial.print(count);
+  
+  // Zero count.
+  count = 0;
 
-    // Print unused stack space in bytes.
-    Serial.print(F(", Unused Stack: "));
-    Serial.print(chUnusedThreadStack(waThread1, sizeof(waThread1)));
-    Serial.print(' ');
-    Serial.print(chUnusedThreadStack(waThread2, sizeof(waThread2)));
-    Serial.print(' ');
-    Serial.print(chUnusedMainStack());
-    #ifdef __arm__
-        // ARM has separate stack for ISR interrupts. 
-        Serial.print(' ');
-        Serial.print(chUnusedHandlerStack());
-    #endif  // __arm__
-    Serial.println();
+  // Print unused stack space in bytes.
+  Serial.print(F(", Unused Stack: "));
+  Serial.print(chUnusedThreadStack(waThread1, sizeof(waThread1)));
+  Serial.print(' ');
+  Serial.print(chUnusedThreadStack(waThread2, sizeof(waThread2)));
+  Serial.print(' ');
+  Serial.print(chUnusedMainStack());
+#ifdef __arm__
+  // ARM has separate stack for ISR interrupts. 
+  Serial.print(' ');
+  Serial.print(chUnusedHandlerStack());
+#endif  // __arm__
+  Serial.println();
 }
