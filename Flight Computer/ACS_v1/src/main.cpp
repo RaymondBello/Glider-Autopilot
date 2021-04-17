0163e34612ea173b4881ffe5d33db35007dc7013
ray <raymondbello11@gmail.com>
2021-04-17 04:50:39 -0400

    new

--- Flight Computer/ACS_v1/src/main.cpp
+++ Flight Computer/ACS_v1/src/main.cpp
@@ -1,98 +1,27 @@
-#include <SoftwareSerial.h>
-#include <Arduino.h>
-#include "Config.h"
-
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-/*
-   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and aSerial8umes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
+
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
-// The serial connection to the GPS device
-void displayInfo()
-{
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
 
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println();
-}
-
+// For stats that happen every 5 seconds
+unsigned long last = 0UL;
 
 void setup()
 {
   Serial.begin(115200);
   Serial8.begin(GPSBaud);
 
-  Serial.println(F("DeviceExample.ino"));
-  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
-  Serial.println();
 }
 
 void loop()
 {
-  // This sketch displays information every time a new sentence is correctly encoded.
+  // Dispatch incoming characters
   while (Serial8.available() > 0)
     Serial.print(char(Serial8.read()));
-    Serial.println(".");
 
-  // if (gps.encode())
-  // displayInfo();
-
-  // if (millis() > 5000 && gps.charsProcessed() < 10)
-  // {
-  //   Serial.println(F("No GPS detected: check wiring."));
-  //   while(true);
-  // }
+  
 }
-
