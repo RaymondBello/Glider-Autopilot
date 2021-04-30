3780406736f1aef149dfcc3718f6ccd878b0085d
ray <raymondbello11@gmail.com>
2021-04-30 01:04:18 -0400

    add uart console to commandView

--- Ground Control Software/Java Telemetry Viewer/src/CommandView.java
+++ Ground Control Software/Java Telemetry Viewer/src/CommandView.java
@@ -1,10 +1,13 @@
 import java.awt.Dimension;
-
+import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import java.awt.Font;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextArea;
 
 
 
@@ -13,26 +16,60 @@ public class CommandView extends JPanel {
    
    static CommandView instance = new CommandView();
 
-   JTextField commandTextField;
-   JButton sendCommandButton;
-   Dimension preferredSize;
+   
+   JTextField commandTextField;  // Box where all telemetry commands to be sent are entered 
+   JButton sendCommandButton;    // Sends the telemetry command specified in 'commandTextField'
+   JButton clearSerialMonitorButton; // Clears serial monitor box
+   JTabbedPane serialMonitorPane;
+   
+   Dimension preferredSize;      // Dimension initialization 
 
    private CommandView() {
       super();
+
+      // Set layout style
       setLayout(new MigLayout());
 
+      // 
       add(new JLabel("Enter Telemetry Command "));
-      commandTextField = new JTextField("",10);
+      commandTextField = new JTextField("", 10);
       add(commandTextField, "grow");
       sendCommandButton = new JButton("Send Command");
-      add(sendCommandButton, "wrap");
+      add(sendCommandButton);
+      clearSerialMonitorButton = new JButton("Clear monitor");
+      add(clearSerialMonitorButton, "wrap");
+
+      // add(Box.createHorizontalStrut(Theme.padding));
+      // tabs for displaying serial Monitor
+      serialMonitorPane = new JTabbedPane();
+      add(serialMonitorPane, "grow, span");
+
 
       preferredSize = getPreferredSize();
+
+      
+      updateSerialMonitor();
 
       setVisible(true);
 
    }
+   
+   public void updateSerialMonitor() {
 
+      serialMonitorPane.removeAll();
 
+      JTextArea serialMonitor = new JTextArea();
+      serialMonitor.setEditable(false);
+      serialMonitor.setTabSize(4);
+      serialMonitor.setFont(new Font("Fira Code", Font.PLAIN, 17));
+
+      serialMonitorPane.add("Serial Monitor Console", new JScrollPane(serialMonitor));
+      serialMonitor.setText("Empty UART Console");
+      serialMonitor.append("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+      // scroll back to the top
+      serialMonitor.setCaretPosition(0);
+
+   }
    
 }
--- Ground Control Software/Java Telemetry Viewer/src/MapView.java
+++ Ground Control Software/Java Telemetry Viewer/src/MapView.java
@@ -1,21 +1,9 @@
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.io.IOException;
-import java.util.Hashtable;
-
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSlider;
-import javax.swing.JTextField;
-import net.miginfocom.swing.MigLayout;
 
 
 @SuppressWarnings("serial")
