0a0e33750cf9e9a341ac53f2502cb822e3e0e6aa
RaymondBello <raymondbello11@gmail.com>
2021-05-23 13:46:52 -0400

    refactor

---
+++ Navigation Computer/nav.py
@@ -0,0 +1,23 @@
+import os
+import logging
+import logging.config
+
+# Set up environment variables
+os.environ["NAV_REPO_PATH"] = os.path.dirname(os.path.realpath(__file__))
+NAV_REPO_PATH = os.environ["NAV_REPO_PATH"]
+
+# Setup nav  
+logging.config.fileConfig(f'{NAV_REPO_PATH}/config/logging.conf')
+nav = logging.getLogger('NavigationSystem')
+
+
+def main():
+    nav.debug(f'debug tyryemessage')
+    nav.info('info message')
+    nav.warning('warn message')
+    nav.error('error message')
+    nav.critical('critical message')
+    
+
+if __name__ == '__main__':
+    main()
